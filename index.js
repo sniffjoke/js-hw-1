2895ef4878e58c6bf0910b336169af94d3e3da07
sniffjoke <sniffjoke@gmail.com>
2024-07-21 18:14:09 +0200

    commit - 4

--- index.js
+++ index.js
@@ -6,8 +6,22 @@
 // console.log(a===c)
 // console.log(c===d)
 
-const a = {}
-const b = a
-b['test']='test value'
-console.log('b: ', b)
-console.log('a: ', a)
+// const a = {}
+// const b = a
+// b['test']='test value'
+// console.log('b: ', b)
+// console.log('a: ', a)
+
+const user = {
+    name: 'John',
+    address: {
+        city: 'New York',
+        country: 'USA',
+    },
+};
+
+const updatedUser = {...user, address: {...user.address, city: 'Toronto'}}
+
+console.log(updatedUser);
+console.log(user);
+
