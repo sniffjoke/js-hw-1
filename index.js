efd71f1f115f7fefaac298a2c43c20f3d0890395
sniffjoke <sniffjoke@gmail.com>
2024-07-21 18:07:16 +0200

    commit - 3

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+// const a = {}
+// const b = {}
+// const c = a
+// const d = c
+// console.log(a===b)
+// console.log(a===c)
+// console.log(c===d)
+
 const a = {}
-const b = {}
-const c = a
-const d = c
-console.log(a===b)
-console.log(a===c)
-console.log(c===d)
+const b = a
+b['test']='test value'
+console.log('b: ', b)
+console.log('a: ', a)
